95a2fbdf706efd28e8ca22d840a7cacb9c24664c
Sahil hirani <sahilhirani01|@gmail.com>
2024-05-16 17:24:46 +0530

    javascr

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ const lowerCase = document.querySelector("#lowercase span");
 const upperCase = document.querySelector("#uppercase span");
 const camelCase = document.querySelector("#camelcase span");
 
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1, str.length);
+}
 function camelCaseOutput(string) {
-  const camelString = camelCaseOutput(string);
-  console.log(camelString);
+  const lowerCaseString = string.toLowerCase();
+  const splittedString = lowerCaseString.split(" ");
+  const finalArr = splittedString.map((word, i) => {
+    if (i === 0) return word;
+    return capitalize(word);
+  });
+  return finalArr.join("");
 }
 
 input.addEventListener("input", (e) => {
   lowerCase.innerText = input.value.toLowerCase();
   upperCase.innerText = input.value.toUpperCase();
+  camelCase.innerText = camelCaseOutput(input.value);
 });
